c7f53fe51ee30f695a092a14a7a2baaa7e467b29
tukno <oliveiradavid007@gmail.com>
2020-02-04 00:36:48 -0400

    chore: Fetching user frm API

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,23 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Keyboard } from 'react-native';
 
 import { Container, Form, Input, SubmitButton } from './styles';
 
-export default function Main() {
-  return (
-    <Container>
-      <Form>
-        <Input
-          autoCorrect={false}
-          autoCapitalize="none"
-          placeholder="Adicionar usuário"
-        />
-        <SubmitButton>
-          <Icon name="add" size={20} color="#fff" />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+import api from '../../services/api';
+
+export default class Main extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      newUser: '',
+      users: [],
+    };
+  }
+
+  handleAddUser = async () => {
+    const { users, newUser } = this.state;
+
+    try {
+      const response = await api.get(`/users/${newUser}`);
+
+      const data = {
+        name: response.data.name,
+        login: response.data.login,
+        bio: response.data.bio,
+        avatar: response.data.avatar_url,
+      };
+
+      this.setState({
+        users: [...users, data],
+        newUser: '',
+      });
+    } catch (error) {
+      console.tron.log('Não foi possível localizar o usuário');
+    }
+
+    // Hide the keyboard after calling the API
+    Keyboard.dismiss();
+  };
+
+  render() {
+    const { users, newUser } = this.state;
+
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Adicionar usuário"
+            value={newUser}
+            onChangeText={text => this.setState({ newUser: text })}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
+          />
+          <SubmitButton onPress={this.handleAddUser}>
+            <Icon name="add" size={20} color="#fff" />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
 
 Main.navigationOptions = {
